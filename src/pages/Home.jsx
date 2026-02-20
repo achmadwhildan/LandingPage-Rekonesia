052630aeab3cd4a5f0adc1055127ecfc9f815ad5
achmadwhildan <achmad.whildan@gmail.com>
2026-02-20 13:21:46 +0700

    navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home-content">
+                <h1>Lorem</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime reprehenderit distinctio quia provident magnam, velit earum mollitia sit odio ea? Similique atque voluptates eius nobis cupiditate dolorum, ut quia tenetur.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
