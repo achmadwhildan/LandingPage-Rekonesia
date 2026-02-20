07aa7aa853fe531354dfa74e6f170cf352addc58
muhamadfadlan <muhamadfadlanhamid@gmail.com>
2026-02-20 15:14:50 +0700

    Apa aja

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
     return (
         <div className="home">
             <div className="home-content">
-                <h1>Lorem</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime reprehenderit distinctio quia provident magnam, velit earum mollitia sit odio ea? Similique atque voluptates eius nobis cupiditate dolorum, ut quia tenetur.</p>
+                <h1>Transforming Organizations Through Technology</h1>
+                <p>Mendorong transformasi digital sektor publik dan swasta melalui solusi teknologi inovatif, tata kelola yang kuat, dan strategi berbasis kinerja untuk menciptakan ekosistem yang berkelanjutan di Indonesia.</p>
             </div>
         </div>
     );
