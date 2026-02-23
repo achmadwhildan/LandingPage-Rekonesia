19ab64b8c85cd0a83431f06bd7546297390cc210
muhamadfadlan <muhamadfadlanhamid@gmail.com>
2026-02-23 21:33:58 +0700

    add-logo-name

--- src/pages/About/Customer.jsx
+++ src/pages/About/Customer.jsx
@@ -2,18 +2,18 @@ import React from "react";
 import "./Customer.css";
 
 const partners = [
-    "/img/BPN.png",
-    "/img/bpsdm.png",
-    "/img/bsk.png",
-    "/img/dishub.png",
-    "/img/djham.png",
-    "/img/imigrasi.png",
-    "/img/ina.png",
-    "/img/kumham.png",
-    "/img/ky.png",
-    "/img/panrb.png",
-    "/img/pemasyarakatan.png",
-    "/img/perdagangan.png",
+    { logo: "/img/BPN.png", name: "Badan Pertanahan Nasional" },
+    { logo: "/img/bpsdm.png", name: "BPSDM" },
+    { logo: "/img/bsk.png", name: "BSK" },
+    { logo: "/img/dishub.png", name: "Dinas Perhubungan" },
+    { logo: "/img/djham.png", name: "Ditjen HAM" },
+    { logo: "/img/imigrasi.png", name: "Imigrasi" },
+    { logo: "/img/ina.png", name: "TNI" },
+    { logo: "/img/kumham.png", name: "Kemenkumham" },
+    { logo: "/img/ky.png", name: "Komisi Yudisial" },
+    { logo: "/img/panrb.png", name: "KemenPAN-RB" },
+    { logo: "/img/pemasyarakatan.png", name: "Pemasyarakatan" },
+    { logo: "/img/perdagangan.png", name: "Kemendag" },
 ];
 
 const Partners = () => {
@@ -22,9 +22,12 @@ const Partners = () => {
             <h3 className="partners-title">Our Customers:</h3>
 
             <div className="partners-grid">
-                {partners.map((logo, index) => (
+                {partners.map((partner, index) => (
                     <div className="partner-card" key={index}>
-                        <img src={logo} alt="Partner Logo" />
+                        <img src={partner.logo} alt={partner.name} />
+                        <div className="partner-overlay">
+                            <span className="partner-name">{partner.name}</span>
+                        </div>
                     </div>
                 ))}
             </div>
